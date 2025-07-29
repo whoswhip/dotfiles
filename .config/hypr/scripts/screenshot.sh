f516f7ed9e5e548877136ccfaf940fe1950a72d6
whoswhip <124531971+whoswhip@users.noreply.github.com>
2025-07-28 20:36:50 -0400

    Delete .config directory

--- .config/hypr/scripts/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET_DIR="/run/media/whip/external drive/Screenshots/LinuxScreenshots"
-
-mkdir -p "$TARGET_DIR"
-
-RANDOM_STRING=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
-
-grim -g "$(slurp -d)" "$TARGET_DIR/$RANDOM_STRING.png" && wl-copy < "$TARGET_DIR/$RANDOM_STRING.png"
